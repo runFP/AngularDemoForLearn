bf9b69cf9fbb68a2680d3ca086a2e097cef3e910
huangjian13 <jian9.huang@meicloud.com>
2020-12-09 18:06:48 +0800

    动画OK

--- src/app/three/component/roller-metal/machines/appendingMachine/AppendingMachine.ts
+++ src/app/three/component/roller-metal/machines/appendingMachine/AppendingMachine.ts
@@ -1,7 +1,15 @@
-import {AnimationClip, AnimationMixer, Clock, Group, LoadingManager, LoopOnce, LoopPingPong, Vector3, VectorKeyframeTrack} from 'three';
+import {
+  AnimationClip,
+  AnimationMixer, Clock,
+  Group,
+  LoadingManager, LoopOnce,
+  LoopPingPong,
+  Vector3,
+  VectorKeyframeTrack
+} from 'three';
 import {Subject, zip} from 'rxjs';
 import {fixedObjLocalOrigin, loadMtlObj} from '../utils';
-import {BaseMachine} from '../baseMachine';
+import {AnimationManager, BaseMachine} from '../baseMachine';
 
 const SHRINK = 100;
 
@@ -28,6 +36,7 @@ const VERTICAL_MAX = 12;
  * 物料机，可上下和左右移动
  */
 export class AppendingMachine extends BaseMachine {
+  name = 'append';
   // 导入的最原始的模型对象（object3D)
   base = null;
   vertical = null;
@@ -35,13 +44,13 @@ export class AppendingMachine extends BaseMachine {
 
   // 已经进行了位置修复的组，组内包含了最原始的模型对象
   baseGroup: Group;
-  verticalGroup: Group = new Group();
-  horizontalGroup: Group = new Group();
+  verticalGroup: Group;
+  horizontalGroup: Group;
 
   // 所有部件的组
-  group = new Group();
+  group: Group;
   // 垂直和上下平移组
-  vhGroup = new Group();
+  vhGroup: Group;
 
   // 上下移动回调
   verticalStart = new Subject();
@@ -56,27 +65,48 @@ export class AppendingMachine extends BaseMachine {
   verticalAction;
   vhAction;
 
+  // 混合/动画控制控制
+  animationManagers: AnimationManager[] = [
+    {name: 'vertical', track: null, action: null, clip: null, mixer: null},
+    // {name: 'vertical', track: null, action: null, clip: null, mixer: new AnimationMixer(this.verticalGroup)},
+    {name: 'vh', track: null, action: null, clip: null, mixer: null},
+  ];
+
   vhActionId = null;
 
   // 是否已初始化
   isInit = false;
 
-  constructor(manager?: LoadingManager) {
-    super(manager);
-  }
-
   camera;
   renderer;
   scene;
+
+  constructor(manager?: LoadingManager) {
+    super(manager);
+
+    this.verticalGroup = new Group();
+    this.horizontalGroup = new Group();
+    this.group = new Group();
+    this.vhGroup = new Group();
+    this.baseGroup = new Group();
+    this.verticalGroup.name = 'verticalGroup';
+    this.horizontalGroup.name = 'horizontalGroup';
+    this.group.name = 'group';
+    this.vhGroup.name = 'vhGroup';
+    this.baseGroup.name = 'baseGroup';
+  }
+
 
   /**
    * 初始化 调用任何操作前必须先执行该方法来加载模型并做相应的初始工作
    * @param shrink 缩放比例
    */
-  init(camera, renderer, scene, shrink: number = 100): Promise<string> {
+  init(camera, renderer, scene, shrink: number = 100): Promise<any> {
     this.camera = camera;
     this.renderer = renderer;
     this.scene = scene;
+
+    this.createAnimation();
     return new Promise(resolve => {
       zip(loadMtlObj(PATH.BASE.MTL_PATH, PATH.BASE.OBJ_PATH, this.manager, SHRINK),
         loadMtlObj(PATH.VERTICAL.MTL_PATH, PATH.VERTICAL.OBJ_PATH, this.manager, SHRINK),
@@ -93,7 +123,7 @@ export class AppendingMachine extends BaseMachine {
             horizontalG.position.set(-5, 6.2, 11.1);
             verticalG.position.set(-5, 10, 11.1);
 
-            this.baseGroup = baseG;
+            this.baseGroup.add(baseG);
             // 偏移后再次添加到group，这样可以相对于目前在base上的位置移动
             this.horizontalGroup.add(horizontalG);
             this.verticalGroup.add(verticalG);
@@ -104,12 +134,80 @@ export class AppendingMachine extends BaseMachine {
             this.isInit = true;
           }, () => {
           }, () => {
-            resolve('complete');
+            resolve(this);
           }
         );
     });
   }
 
+  private createAnimation() {
+    this.createHorizontalAnimation();
+    this.createVerticalAnimation();
+  }
+
+  private createHorizontalAnimation() {
+    const times = [];
+    const values = [];
+    const tmp = new Vector3();
+    const duration = 3;
+
+    for (let i = 0, j = duration * 10 ; i <= duration * 10; i++, j--) {
+      times.push(i / 10);
+      if (i > duration * 10 / 2) {
+        tmp.setX(j).toArray(values, values.length);
+      } else {
+        tmp.setX(i).toArray(values, values.length);
+      }
+    }
+
+    const mixer = new AnimationMixer(this.vhGroup);
+    const track = new VectorKeyframeTrack('.position', times, values);
+    const clip = new AnimationClip('vhMove', duration, [track]);
+    const action = mixer.clipAction(clip);
+    action.clampWhenFinished = true;
+    action.loop = LoopOnce;
+    mixer.addEventListener('finished', () => {
+      this.playVertical();
+      this.horizontalEnd.next(this.vhGroup);
+    });
+    Object.assign(this.getAnimationManager('vh'), {track, clip, action, mixer});
+  }
+
+  private createVerticalAnimation() {
+    const times = [];
+    const values = [];
+    const tmp = new Vector3();
+    const duration = 1.6;
+
+    for (let i = 0, j = duration * 10; i < duration * 10; i++, j--) {
+      times.push(i / 10);
+      if (i > duration * 10 / 2) {
+        tmp.setY(-j).toArray(values, values.length);
+      } else {
+        tmp.setY(-i).toArray(values, values.length);
+      }
+    }
+
+    const mixer = new AnimationMixer(this.verticalGroup);
+    const track = new VectorKeyframeTrack('.position', times, values);
+    const clip = new AnimationClip('verticalMove', duration, [track]);
+    const action = mixer.clipAction(clip);
+    action.clampWhenFinished = true;
+    action.loop = LoopOnce;
+    mixer.addEventListener('finished', () => {
+      this.playHorizontal();
+      this.horizontalEnd.next(this.horizontalGroup);
+    });
+    Object.assign(this.getAnimationManager('vertical'), {track, clip, action, mixer});
+  }
+
+  private getAnimationManager(name: string): AnimationManager {
+    const matchAm = this.animationManagers.find(am => am.name === name);
+    if (!matchAm) {
+      throw new Error('动画管理器名字不匹配');
+    }
+    return matchAm;
+  }
 
   /**
    * 上下移动
@@ -126,68 +224,22 @@ export class AppendingMachine extends BaseMachine {
   /**
    * 左右移动
    */
-  startHorizontal(duration = 1.2) {
-    if (this.vhAction) {
-      if (this.vhAction.paused === true) {
-        this.vhAction.paused = false;
-        return;
-      } else if (this.vhActionId !== null) {
-        this.vhAction.play();
-        return;
-      }
-    }
-
-    const times = [];
-    const values = [];
-    const tmp = new Vector3();
-
-    for (let i = 0; i < duration * 10; i++) {
-      times.push(i / 10);
-      tmp.setX(i).toArray(values, values.length);
-    }
-    const track = new VectorKeyframeTrack('.position', times, values);
-    const clip = new AnimationClip('horizontalMove', duration, [track]);
-    const mixer = new AnimationMixer(this.vhGroup);
-    this.vhAction = mixer.clipAction(clip);
-    this.vhAction.play();
-    this.vhAction.loop = LoopPingPong;
-
-    // action.loop = LoopOnce;
+  playHorizontal(duration = 1.2) {
     this.horizontalStart.next(this.horizontalGroup);
-    this.horizontalEnd.next(this.horizontalGroup);
-
-    const clock = new Clock();
-    const animate = () => {
-      this.vhActionId = requestAnimationFrame(animate);
-      if (mixer) {
-        mixer.update(clock.getDelta());
-      }
-      this.render();
-    };
-    animate();
+    const action = this.getAnimationManager('vh').action;
+    action.reset().play();
   }
+
 
   /**
-   * 停止动画，动画会重置
+   * 左右移动
    */
-  stopHorizontal() {
-    this.vhAction.stop();
+  playVertical(duration = 0.8) {
+    this.horizontalStart.next(this.horizontalGroup);
+    const action = this.getAnimationManager('vertical').action;
+    action.reset().play();
   }
 
-  /**
-   * 暂停动画，不会重置
-   */
-  pauseHorizontal() {
-    this.vhAction.paused = true;
-  }
-
-  cancelHorizontal() {
-    if (this.vhActionId !== null) {
-      cancelAnimationFrame(this.vhActionId);
-      this.vhAction = null;
-      this.vhActionId = null;
-    }
-  }
 
   /** 动画相关方法结束 */
 
@@ -205,5 +257,5 @@ export class AppendingMachine extends BaseMachine {
     this.renderer.render(this.scene, this.camera);
   }
 
-
 }
+
--- src/app/three/component/roller-metal/machines/baseMachine.ts
+++ src/app/three/component/roller-metal/machines/baseMachine.ts
@@ -1,9 +1,24 @@
-import {LoadingManager} from 'three';
+import {AnimationAction, AnimationClip, AnimationMixer, KeyframeTrack, LoadingManager} from 'three';
 
 export class BaseMachine {
+  name: string;
   manager: LoadingManager | null;
+  animationManagers: AnimationManager[];
 
   constructor(manager?: LoadingManager) {
     this.manager = manager;
   }
+
+  init(...args): Promise<any> {
+    return new Promise<any>(resolve => {
+    });
+  }
+}
+
+export interface AnimationManager {
+  name: string;
+  track: KeyframeTrack;
+  mixer: AnimationMixer;
+  clip: AnimationClip;
+  action: AnimationAction;
 }
